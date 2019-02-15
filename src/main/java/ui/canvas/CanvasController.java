20b95e4c916b4c0762702cf54951795d28ffc064
Allen Qian <qianyilun@gmail.com>
2019-02-14 22:05:12 -0800

    grid ui can be updated

--- src/main/java/ui/canvas/CanvasController.java
+++ src/main/java/ui/canvas/CanvasController.java
@@ -2,6 +2,9 @@ package ui.canvas;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.text.Text;
 import model.Status;
 import ui.register.CanvasModel;
@@ -22,6 +25,7 @@ public class CanvasController {
     public TextArea boxPercentToColor;
     public Label penSettingLabel;
     public Button updateCanvasBtn;
+    public GridPane canvasGridPane;
 
     public void onUpdateCanvasBtnClicked(ActionEvent event) {
         if (Status.getInstance().isInGame()) {
@@ -38,6 +42,8 @@ public class CanvasController {
                 Status.getInstance().setCanvasModel(canvasModel);
 
                 System.out.println("Canvas Model has been set to " + Status.getInstance().getCanvasModel());
+
+                updateCanvasGridPanel(canvasModel);
             } else {
                 throw new NumberFormatException();
             }
@@ -46,12 +52,40 @@ public class CanvasController {
 
         } catch (NumberFormatException e) {
             e.printStackTrace();
-            
+
             System.out.println("Please enter valid canvas settings");
             boxRow.clear();
             boxColumn.clear();
             boxPercentToColor.clear();
         }
+    }
+
+    private void updateCanvasGridPanel(CanvasModel canvasModel) {
+        cleanCanvasGridPanel();
+
+        int numRows = canvasModel.getCanvasRow();
+        for (int i = 0; i < numRows; i++) {
+            RowConstraints rowConst = new RowConstraints();
+            rowConst.setPercentHeight(100.0 / numRows);
+            canvasGridPane.getRowConstraints().add(rowConst);
+        }
+
+        int numCols = canvasModel.getCanvasColumn();
+        for (int i = 0; i < numCols; i++) {
+            ColumnConstraints colConst = new ColumnConstraints();
+            colConst.setPercentWidth(100.0 / numCols);
+            canvasGridPane.getColumnConstraints().add(colConst);
+        }
+    }
+
+    private void cleanCanvasGridPanel() {
+        while(canvasGridPane.getRowConstraints().size() > 0){
+            canvasGridPane.getRowConstraints().remove(0);
+        }
+        while(canvasGridPane.getColumnConstraints().size() > 0){
+            canvasGridPane.getColumnConstraints().remove(0);
+        }
+
     }
 
     public void onReadyClicked(ActionEvent event) {
