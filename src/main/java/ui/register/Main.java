b9d98875f36f8bef1bea10549b663a6406b3ad73
Allen Qian <qianyilun@gmail.com>
2019-03-09 21:44:27 -0800

    update register ui in create tab

--- src/main/java/ui/register/Main.java
+++ src/main/java/ui/register/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/register.fxml"));
         primaryStage.setTitle("deny-and-conquer-register");
-        primaryStage.setScene(new Scene(root, 350, 400));
+        primaryStage.setScene(new Scene(root, 350, 500));
         primaryStage.getScene();
 
         primaryStage.show();
